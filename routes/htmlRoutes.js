6e650179f747857781de7f6ba9d24e74f34768b2
Andrew Circelli <andrewcircelli@gmail.com>
2020-11-09 14:15:21 -0500

    htmlRoutes set up

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,10 +1,17 @@
+// establish the dependencies
 const path = require("path");
 const router = require("express").Router();
 
+// notes html rout 
 router.get("/notes", (req, res) => {
-    // here you will render the notes HTML page
-})
+    // render the notes HTML page
+    res.sendFile(path.join(__dirname, '../public/', '/notes.html'));
+});
 
+// all other html routes 
 router.get("*", (req, res) => {
-    // this is the default route where you will render index.html
-})
+    // default route to render index.html
+    res.sendFile(path.join(__dirname, '../public/', '/index.html'));
+});
+
+module.exports = router;
